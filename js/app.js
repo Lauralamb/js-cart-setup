c0226e6efbb25652b558cf565a563ff6a1057bd7
Laura Lamb <jklm1304@gmail.com>
2019-02-05 18:58:40 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//show cart
+
+(function(){
+const cartInfo = document.getElementById('cart-info');
+const cart = document.getElementById('cart');
+
+cartInfo.addEventListener('click', function(){
+    cart.classList.toggle('show-cart');
+})
+})();
+
+//add items to the cart
+
+(function(){
+const cartBtn = document.querySelectorAll('.store-item-icon');
+
+
+
+
+})();
